d95ef45bd47f006d82fa96e6010ef3cffb16385c
s_kravchenya <svetlankravec@yandex.ru>
2023-03-30 22:48:47 +0300

    hw3

---
+++ hw3-1/script.js
@@ -0,0 +1,15 @@
+document.querySelector('#cels').onclick = celsToF;
+document.querySelector('#your_name').onclick = greeting;
+
+function celsToF() {
+    let cels = document.querySelector('.cels').value;
+    let far = Number(cels) * (9/5) + 32
+    document.write('Цельсиев:' + cels)
+    document.write('Фаренгейтов:' + far.toFixed(1))
+}
+
+function greeting() {
+    let name = document.querySelector('.name').value;
+    console.log('Здарова, ' + name);
+    document.write('Здарова, ' + name)
+}
--- hw4/script.js
+++
@@ -1,8 +0,0 @@
-document.querySelector('button').onclick = nameOk;
-
-function nameOk() {
-    console.log('name ok');
-    let name = document.querySelector('.name').value;
-    console.log(name);
-    document.querySelector('.output').innerHTML = name;
-}
