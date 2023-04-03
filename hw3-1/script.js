1914525c9184c8027b147277f8e1bf443e633712
skravchenya42 <118940046+skravchenya42@users.noreply.github.com>
2023-04-03 22:25:02 +0300

    Update script.js

--- hw3-1/script.js
+++ hw3-1/script.js
@@ -2,8 +2,8 @@ document.querySelector('#cels').onclick = celsToF;
 document.querySelector('#your_name').onclick = greeting;
 
 function celsToF() {
-    let cels = document.querySelector('.cels').value;
-    let far = Number(cels) * (9/5) + 32
+    let cels = +document.querySelector('.cels').value;
+    let far = Number(cels) * (9 / 5) + 32
     document.write('Цельсиев:' + cels)
     document.write('Фаренгейтов:' + far.toFixed(1))
 }
